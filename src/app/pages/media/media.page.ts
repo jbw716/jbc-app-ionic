88e40cea5d89a3cd769e5a044f701fd05ea756c6
Jeremiah Weaver <jbw71699@gmail.com>
2019-06-16 08:02:43 -0500

    Initital commit and project setup. Created app pages and set up shared module.

---
+++ src/app/pages/media/media.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jbc-media',
+  templateUrl: './media.page.html',
+  styleUrls: ['./media.page.scss'],
+})
+export class MediaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
